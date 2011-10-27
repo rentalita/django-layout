f68616793cb0fb19e9ec5edd6f39fcb92c3eb9f2
Tom Vaughan <tom@software6.net>
2011-10-27 17:59:30 -0300

    add restart command (instead of start & stop)

--- django-layout/bin/lighttpd.sh
+++ django-layout/bin/lighttpd.sh
@@ -7,19 +7,23 @@ export @OVID@_HOME
 
 print_usage_and_die()
 {
-    echo "Usage: $(basename $0) [start | stop]"
+    echo "Usage: $(basename $0) [start | stop | restart]"
     exit 1
 }
 
 OPTION="$1"
 
 case "${OPTION}" in
-    *start)
+    start)
 	    lighttpd -f "${@OVID@_ETC}"/lighttpd.conf
 	;;
-    *stop)
+    stop)
 	    [ -f "${@OVID@_RUN}"/lighttpd.pid ] && \
 	        kill $(cat "${@OVID@_RUN}"/lighttpd.pid)
+	;;
+    restart)
+            "$0" stop
+            "$0" start
 	;;
     *)
 	    print_usage_and_die
